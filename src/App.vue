99610f413e70611cf352f4ff0cb3fb1e831ca1d7
SimoneCappadona <simocappa95@gmail.com>
2024-02-08 15:35:44 +0100

    small fix

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
-<script >
-import generalscss from './style/general.scss'
+<script>
+import AppHeader from './components/appHeader.vue';
+import AppMain from './components/appMain.vue';
+import AppFooter from './components/appFooter.vue';
+export default{
+    data(){
+        return {
+
+        };
+    },
+    components : {AppHeader,AppMain,AppFooter }
+};
+
 </script>
 
 <template>
-  
+  <AppHeader></AppHeader>
+  <AppMain></AppMain>
+  <AppFooter></AppFooter>
 </template>
 
-<style @use>
+<style lang="scss">
+@use './style/general.scss';
 </style>
